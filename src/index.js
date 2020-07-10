6f2a8899ebdd74fd67be1350dae67303f82ea2e0
yuanChuan <berserk5683@gmail.com>
2020-07-10 17:17:59 +0800

    项目init

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Three from './pages/three';
+
+
+const Index = () => {
+  return <div><Three /></div>;
+};
+
+ReactDOM.render(<Index />, document.getElementById('index'));
